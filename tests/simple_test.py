e91ba9f431f76ad2d30c2cabfa39088bd15cbe15
Thomas Briedigkeit <thomas.briedigkeit@fokus.fraunhofer.de>
2018-05-07 12:36:01 +0200

    Imp: Add tests module and travis file

---
+++ tests/simple_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    # TODO add real tests
+    def test_something(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
